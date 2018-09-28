fab31abe4537a7c66a5154e5d5bfa9a115a43844
lakshmi.dontamsetti@oracle.com <lakshmi.dontamsetti@oracle.com>
2018-09-28 15:03:42 -0700

    initial commit

---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,16 @@
+const val API_KEY = "abdfkdfgl453"
+class Helper {
+    fun getSum(first: Int, second: Int): Int = first + second
+    fun sliceFilterAndSort(list: List<String>): List<String> =
+            list.subList(0, 4).filter { it.length > 3 }.sortedBy { it.length }
+
+    companion object {
+        val helperId: Int = 0
+        fun getHelperType() : String = "Helper234"
+    }
+}
+
+data class Model(
+        var id: Int = 0,
+        var type: String = ""
+)
